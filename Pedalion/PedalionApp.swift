11f57df390795cc18868f58f8184c2c593e414f4
Sajan Lawrence <sajanlawrence12345@gmail.com>
2025-12-21 12:44:27 +0530

    Initial Commit

---
+++ Pedalion/PedalionApp.swift
@@ -0,0 +1,17 @@
+//
+//  PedalionApp.swift
+//  Pedalion
+//
+//  Created by Sajan Lawrence on 21/12/25.
+//
+
+import SwiftUI
+
+@main
+struct PedalionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
